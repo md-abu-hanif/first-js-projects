fe30810844c9160c9677fd5f0f98a24b02837239
md-abu-hanif <mdabuhanif5811@gmail.com>
2021-08-21 22:43:40 +0600

    final

--- JS/app.js
+++ JS/app.js
@@ -1,82 +1,78 @@
+// Button Function //
+function updatePrice(product, price) {
+    const itemPrice = document.getElementById(product + '-cost')
+    itemPrice.innerText = price;
+    calculateTotal();
+}
+//  Button //
 document.getElementById('memory-btn1').addEventListener('click', function () {
-    const memory = document.getElementById('memory-btn1').value;
-    const value = parseInt(memory + '0');
-
-    document.getElementById('memory-cost').innerText = value;
-
+    updatePrice('memory', 0);
 })
-
-
 document.getElementById('memory-btn2').addEventListener('click', function () {
-    const memory = document.getElementById('memory-btn2').value;
-    const value = parseInt(memory + '180');
-
-    document.getElementById('memory-cost').innerText = value;
-
-
-
+    updatePrice('memory', 180);
 })
-
-
-
-
-
 document.getElementById('storage-btn1').addEventListener('click', function () {
-    const memory = document.getElementById('storage-btn1').value;
-    const value = parseInt(memory + '0');
-
-    document.getElementById('storage-cost').innerText = value;
-
+    updatePrice('storage', 0);
 })
 document.getElementById('storage-btn2').addEventListener('click', function () {
-    const memory = document.getElementById('storage-btn2').value;
-    const value = parseInt(memory + '100');
-
-    document.getElementById('storage-cost').innerText = value;
-
+    updatePrice('storage', 100);
 })
 document.getElementById('storage-btn3').addEventListener('click', function () {
-    const memory = document.getElementById('storage-btn3').value;
-    const value = parseInt(memory + '180');
-
-    document.getElementById('storage-cost').innerText = value;
-
+    updatePrice('storage', 180);
 })
-
-
-
-
-
-
-
-
-
 document.getElementById('delivery-btn1').addEventListener('click', function () {
-    const memory = document.getElementById('delivery-btn1').value;
-    const value = parseInt(memory + '0');
-
-    document.getElementById('delivery-cost').innerText = value;
-
+    updatePrice('delivery', 0);
 })
 document.getElementById('delivery-btn2').addEventListener('click', function () {
-    const memory = document.getElementById('delivery-btn2').value;
-    const value = parseInt(memory + '20');
-
-    document.getElementById('delivery-cost').innerText = value;
-
+    updatePrice('delivery', 20);
 })
+//  Button //
 
+//  Calculation Start //
+function getInput(product) {
+    const inputValue = document.getElementById(product + '-cost').innerText;
+    const value = parseInt(inputValue);
+    return value;
+}
+function calculateTotal() {
+    const basePrice = getInput('base');
+    const memoryPrice = getInput('memory');
+    const storagePrice = getInput('storage');
+    const deliveryPrice = getInput('delivery');
 
-function conclusion() {
-    const delivery = document.getElementById('delivery-cost').innerText;
-    const storage = document.getElementById('storage-cost').innerText;
-    const memory = document.getElementById('storage-cost').innerText;
-
-    const total = delivery + storage + memory;
-
-    console.log(total);
-
+    const total = basePrice + memoryPrice + storagePrice + deliveryPrice;
     document.getElementById('total-cost').innerText = total;
-
+    document.getElementById('total').innerText = total;
 
 }
+//  Calculation End //
+
+
+//  Promo Start //
+document.getElementById('promo-code').addEventListener('click', function () {
+    const input = document.getElementById('promo-input').value;
+    const code = 'stevekaku';
+    const total = document.getElementById('total-cost').innerText;
+    const success = document.getElementById('promo-success');
+    const fail = document.getElementById('promo-error');
+    document.getElementById('promo-input').value = '';
+    if (input == code) {
+        const last = parseFloat(total - ((total / 100) * 20));
+        document.getElementById('total').innerText = last;
+
+    }
+
+})
+// promo End //
+
+
+
+
+
+
+
+
+
+
+
+
